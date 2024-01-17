f3a1300c166f60f77c2e9b87f583253b1aec994e
Colin <colin.hippel@web.de>
2024-01-17 17:47:19 +0100

    changes nearly everyting

--- src/App.js
+++ src/App.js
@@ -1,45 +1,51 @@
-function App() {
-    return (
-
-        <><div id="result"></div>
+import React, { Component } from 'react';
 
 
-            <table>
-                <tr>
-                    <td onClick="appendOperation(7)">7</td>
-                    <td onclick="appendOperation(8)">8</td>
-                    <td onclick="appendOperation(9)">9</td>
-                    <td onclick="appendOperation(' + ')" className="operator">+</td>
-                    <td onclick="deleteLast()" id="clear-button">C</td>
-                </tr>
+class ButtonPanel extends Component {
+    render() {
+        return (
+            <>
+                <div id='full-grid'>
+                    <div id='result'>123</div>
 
-                <tr>
-                    <td onclick="appendOperation(4)">4</td>
-                    <td onclick="appendOperation(5)">5</td>
-                    <td onclick="appendOperation(6)">6</td>
-                    <td onclick="appendOperation(' - ')" className="operator">-</td>
-                </tr>
-
-                <tr>
-                    <td onclick="appendOperation(1)">1</td>
-                    <td onclick="appendOperation(2)">2</td>
-                    <td onclick="appendOperation(3)">3</td>
-                    <td onclick="appendOperation(' / ')" className="operator">/</td>
-                </tr>
-                <tr>
-                    <td onclick="appendOperation(0)">0</td>
-                    <td onclick="appendOperation('.')" className="operator">.</td>
-                    <td onclick="calculate()" id="result-button">=</td>
-                    <td onclick="appendOperation(' * ')" className="operator">*</td>
-                </tr>
-
-            </table></>
-    );
+                    <div className="component-button-panel">
+                        <div>
+                            <button id='clear-button'>C</button>
+                            <button id='operator'>/</button>
+                        </div>
+                        <div>
+                            <button>7</button>
+                            <button>8</button>
+                            <button>9</button>
+                            <button id='operator'>*</button>
+                        </div>
+                        <div>
+                            <button>4</button>
+                            <button>5</button>
+                            <button>6</button>
+                            <button id='operator'>-</button>
+                        </div>
+                        <div>
+                            <button>1</button>
+                            <button>2</button>
+                            <button>3</button>
+                            <button id='operator'>+</button>
+                        </div>
+                        <div>
+                            <button>0</button>
+                            <button>,</button>
+                            <button id='result-button'>=</button>
+                        </div>
+                    </div>
+                </div>
+            </>
+        );
+    }
 }
 
+export default ButtonPanel;
 
 
 
 
 
-export default App
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import './App.css'
-import App from './App';
-
+import ButtonPanel from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <ButtonPanel />
 );
